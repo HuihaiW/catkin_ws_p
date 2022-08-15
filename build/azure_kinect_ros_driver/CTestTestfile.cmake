a40e6f04050d9e54f2456539da765d4d388e59a8
huihaiW <hwang164@binghamton.edu>
2022-08-15 11:02:33 -0500

    add azure kinect ros driver

---
+++ build/azure_kinect_ros_driver/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/huihai/catkin_ws_p/src/Azure_Kinect_ROS_Driver
+# Build directory: /home/huihai/catkin_ws_p/build/azure_kinect_ros_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ devel/share/azure_kinect_ros_driver/cmake/azure_kinect_ros_driverConfig.cmake
@@ -0,0 +1,1 @@
+/home/huihai/catkin_ws_p/devel/.private/azure_kinect_ros_driver/share/azure_kinect_ros_driver/cmake/azure_kinect_ros_driverConfig.cmake
